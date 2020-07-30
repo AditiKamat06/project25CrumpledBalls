f2a49241edf92e6005ca1467cc476a7b030cb3d6
AditiKamat06 <65010562+AditiKamat06@users.noreply.github.com>
2020-07-30 16:37:57 +0530

    Add files via upload

---
+++ project 24 crumpled balls/balls.js
@@ -0,0 +1,34 @@
+class Balls {
+  constructor(x, y,diametre) {
+    var options = {
+        'restitution':0.5,
+        'friction':1.0,
+        'density':1.5
+    } 
+    this.x = x;
+    this.y = y;
+    this.diametre=diametre;
+    this.body = Bodies.circle(x, y, diametre/2, options);
+
+    this.image=loadImage("sprites/tissuePaper.png");
+
+
+    World.add(world, this.body);
+  }
+
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+
+    image(this.image, this.body.position.x, this.body.position.y, 70, 70);
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    ellipseMode(RADIUS);
+    fill(255);
+    strokeWeight(4);
+    ellipse(0, 0, this.diametre, this.diametre);
+    pop();
+  }
+} 
